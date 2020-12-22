c810cdbe1a1698a8a54b5076597b9d9a1a07819f
hjh <hujh1997@126.com>
2020-12-22 21:25:56 +0800

    hello-rrr

--- javams/src/hello/Hello.java
+++ javams/src/hello/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("hello");
         System.out.println("ddddd");
         System.out.println("dddd");
+        System.out.println("rrrr    ");
     }
 }
