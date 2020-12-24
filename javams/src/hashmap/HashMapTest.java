25ad7787869c994c469da18648ba0b110a35f253
hjh <hujh1997@126.com>
2020-12-24 13:32:33 +0800

    map-test

---
+++ javams/src/hashmap/HashMapTest.java
@@ -0,0 +1,12 @@
+package hashmap;
+
+import java.util.HashMap;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+        HashMap hashMap = new HashMap();
+        hashMap.put("1","2");
+        System.out.println(hashMap.get("1"));
+        System.out.println("11111");
+    }
+}
